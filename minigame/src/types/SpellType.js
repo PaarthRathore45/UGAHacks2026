fbdabde892981ee6c138659dd891aa3820803dc5
PaarthRathore45 <rathorepaarth@gmail.com>
2026-02-07 17:20:17 -0500

    Updated Files

--- minigame/src/types/SpellType.js
+++
@@ -1,7 +0,0 @@
-export var SpellType;
-(function (SpellType) {
-    SpellType["FIRE"] = "fire";
-    SpellType["WATER"] = "water";
-    SpellType["EARTH"] = "earth";
-    SpellType["AIR"] = "air";
-})(SpellType || (SpellType = {}));
